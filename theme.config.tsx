c3c244c69ea42363a56d20cc92478e4bf37e25b1
SFTechnologies <93928478+SFTechnologies@users.noreply.github.com>
2023-11-22 13:14:27 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>SPECTRIFY</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/SFTechnologies/spectrify-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/SFTechnologies/spectrify-docs',
   footer: {
     text: 'Nextra Docs Template',
   },
