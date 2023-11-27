c0bd619002a9a6f7c0b68250504556353492ee7b
RParrilla <rparrilla@sftec.es>
2023-11-27 09:55:27 +0100

    spectra-management-content

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,8 @@
+import Image from 'next/image';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>SPECTRIFY</span>,
+  logo: <span><Image src="/images/spectrify_logo.svg" alt="spectrify logo" width={200} height={110} /></span>,
   project: {
     link: 'https://github.com/SFTechnologies/spectrify-docs',
   },
@@ -16,3 +17,7 @@ const config: DocsThemeConfig = {
 }
 
 export default config
+
+
+
+
