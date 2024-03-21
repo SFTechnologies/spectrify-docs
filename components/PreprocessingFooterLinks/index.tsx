af15ea31f4c442e2c63995ca9bb30e2b6033f4fb
Raúl Marín <rmarin82@gmail.com>
2024-03-21 14:56:44 +0100

    refactor(PreprocessingFooterLinks): extract reusable component PreprocessingFooterLinks

---
+++ components/PreprocessingFooterLinks/index.tsx
@@ -0,0 +1,70 @@
+import ImageLink from '../ImageLink';
+
+import styles from './PreprocessingFooterLinks.module.css';
+
+interface PreprocessingFooterLinksProps {
+  imagePath: string;
+}
+
+const PreprocessingFooterLinks = ({
+  imagePath,
+}: PreprocessingFooterLinksProps) => {
+  return (
+    <div className={styles.container}>
+      <ImageLink
+        href='/preprocessing/standardization'
+        image={{
+          alt: 'standardization icon',
+          src: `${imagePath}standardization_icon.svg`,
+        }}
+        text='Standardize'
+      />
+      <ImageLink
+        href='/preprocessing/baseline-correction'
+        image={{
+          alt: 'baseline correction icon',
+          src: `${imagePath}baseline_icon.svg`,
+        }}
+        text='Baseline'
+      />
+      <ImageLink
+        href='/preprocessing/smoothing'
+        image={{ alt: 'smoothing icon', src: `${imagePath}smoothing_icon.svg` }}
+        text='Smooth'
+      />
+      <ImageLink
+        href='/preprocessing/offset'
+        image={{ alt: 'offset icon', src: `${imagePath}offset_icon.svg` }}
+        text='Offset'
+      />
+      <ImageLink
+        href='/preprocessing/normalization'
+        image={{
+          alt: 'normalization icon',
+          src: `${imagePath}normalization_icon.svg`,
+        }}
+        text='Normalize'
+      />
+      <ImageLink
+        href='/preprocessing/peak-detection'
+        image={{
+          alt: 'peak detection icon',
+          src: `${imagePath}peak_detection_icon.svg`,
+        }}
+        text='Peaks'
+      />
+      <ImageLink
+        href='/preprocessing/offset'
+        image={{ alt: 'offset icon', src: `${imagePath}derivative_icon.svg` }}
+        text='Derivative'
+      />
+      <ImageLink
+        href='/preprocessing/restore'
+        image={{ alt: 'restore icon', src: `${imagePath}restore_icon.svg` }}
+        text='Restore Spectra'
+      />
+    </div>
+  );
+};
+
+export default PreprocessingFooterLinks;
